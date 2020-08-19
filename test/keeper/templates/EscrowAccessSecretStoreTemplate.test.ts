cb7b5feedf03f56cdcdd8949ec9ee5760d7f5a26
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-08-19 11:07:47 +0200

    activate test

--- test/keeper/templates/EscrowAccessSecretStoreTemplate.test.ts
+++ test/keeper/templates/EscrowAccessSecretStoreTemplate.test.ts
@@ -3,22 +3,28 @@ import { EscrowAccessSecretStoreTemplate } from '../../../src/keeper/contracts/t
 import config from '../../config'
 import TestContractHandler from '../TestContractHandler'
 import { Nevermined } from '../../../src/nevermined/Nevermined'
+import { Condition } from "../../../src"
 
-let condition: EscrowAccessSecretStoreTemplate
+let template: EscrowAccessSecretStoreTemplate
+let condition: Condition
 
 describe('EscrowAccessSecretStoreTemplate', () => {
     before(async () => {
         const nevermined: Nevermined = await Nevermined.getInstance(config)
         await TestContractHandler.prepareContracts()
-        condition = nevermined.keeper.templates.escrowAccessSecretStoreTemplate
+        template = nevermined.keeper.templates.escrowAccessSecretStoreTemplate
+        const conditions = await template.getConditions()
+        condition = conditions.find(
+            (condition) => condition.contractName === 'LockRewardCondition'
+        )
     })
 
-    // describe("#hashValues()", () => {
-    //     it("should hash the values", async () => {
-    //         const address = `0x${"a".repeat(40)}`
-    //         const hash = await condition.hashValues(address, 15)
+    describe("#hashValues()", () => {
+        it("should hash the values", async () => {
+            const address = `0x${"a".repeat(40)}`
+            const hash = await condition.hashValues(address, 15)
 
-    //         assert.match(hash, /^0x[a-f0-9]{64}$/i)
-    //     })
-    // })
+            assert.match(hash, /^0x[a-f0-9]{64}$/i)
+        })
+    })
 })
