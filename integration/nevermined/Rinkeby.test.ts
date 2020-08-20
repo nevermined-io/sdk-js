a9b1a9200058861aa47e2500297beb746ad9958f
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-08-20 15:56:11 +0200

    added integration test for rinkeby

---
+++ integration/nevermined/Rinkeby.test.ts
@@ -0,0 +1,13 @@
+import { assert } from 'chai'
+import { Config, Nevermined } from "../../src"
+import { LogLevel } from "../../src/utils"
+
+describe('Rinkeby', () => {
+    it('should instanciate on rinkeby', async () => {
+        const nvm = await Nevermined.getInstance({
+            nodeUri: "https://rinkeby.infura.io/v3/5b322298e356423ea24362fc80fabd16",
+            verbose: LogLevel.Verbose
+        } as Config)
+        assert.exists(nvm)
+    })
+})
