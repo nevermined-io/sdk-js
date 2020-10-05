d7d120730a46249cc64b575087966d5aadbf9c09
enrique <enrique@keyko.io>
2020-10-05 11:21:45 +0200

    Verbose false in test

--- integration/config.ts
+++ integration/config.ts
@@ -7,7 +7,7 @@ const configJson: Config = {
     gatewayUri: 'http://localhost:8030',
     secretStoreUri: 'http://localhost:12001',
     gatewayAddress: '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0',
-    verbose: true
+    verbose: false
 }
 
 if (process.env.NETWORK_NAME === 'production') {
