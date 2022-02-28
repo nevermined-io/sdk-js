45403f69424df1d324efaf94b74bd769a9e54e34
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-02-28 18:56:45 +0100

    Fix bignumber asserts in the unit tests

--- test/keeper/ContractEvent.test.ts
+++ test/keeper/ContractEvent.test.ts
@@ -4,29 +4,33 @@ import config from '../config'
 import TestContractHandler from './TestContractHandler'
 import { ContractEventSubscription } from '../../src/events/NeverminedEvent'
 import { Account } from '../../src'
+import Web3Provider from '../../src/keeper/Web3Provider'
+import Web3 from 'web3'
 
 describe('ContractEvent', () => {
     let nevermined: Nevermined
     let account1: Account
     let account2: Account
     let account3: Account
+    let account4: Account
+    let web3: Web3
 
     beforeEach(async () => {
         await TestContractHandler.prepareContracts()
         nevermined = await Nevermined.getInstance(config)
-        ;[account1, account2, account3] = await nevermined.accounts.list()
-    })
-
-    afterEach(async () => {
-        await new Promise(resolve => setTimeout(resolve, 50))
+        web3 = Web3Provider.getWeb3(config)
+        ;[account1, account2, account3, account4] = await nevermined.accounts.list()
     })
 
     describe('#subscribe()', () => {
         it('should be able to listen to events', async () => {
             let subscription: ContractEventSubscription
+            const fromBlock = await web3.eth.getBlockNumber()
+
             const waitForEvents = new Promise(resolve => {
                 subscription = nevermined.keeper.token.events.subscribe(
                     events => {
+                        console.log(events)
                         assert.isDefined(events)
                         assert.isAtLeast(events.length, 1)
 
@@ -37,15 +41,19 @@ describe('ContractEvent', () => {
                     },
                     {
                         eventName: 'Transfer',
-                        filterJsonRpc: {},
-                        fromBlock: 0,
+                        filterJsonRpc: {
+                            to: [account1.getId(), account2.getId(), account3.getId()]
+                        },
+                        fromBlock: fromBlock,
                         toBlock: 'latest'
                     }
                 )
             })
+            console.log('before request tokens')
             await nevermined.keeper.dispenser.requestTokens(1, account1.getId())
             await nevermined.keeper.dispenser.requestTokens(2, account2.getId())
             await nevermined.keeper.dispenser.requestTokens(3, account3.getId())
+            console.log('after request tokens')
 
             await waitForEvents
             subscription.unsubscribe()
@@ -54,13 +62,15 @@ describe('ContractEvent', () => {
 
     describe('#once()', () => {
         it('should listen to event only once', async () => {
+            const fromBlock = await web3.eth.getBlockNumber()
+
             const eventsPromise = nevermined.keeper.token.events.once(e => e, {
                 eventName: 'Transfer',
-                filterJsonRpc: { to: account1.getId() },
-                fromBlock: 0,
+                filterJsonRpc: { to: account4.getId() },
+                fromBlock: fromBlock,
                 toBlock: 'latest'
             })
-            await nevermined.keeper.dispenser.requestTokens(1, account1.getId())
+            await nevermined.keeper.dispenser.requestTokens(1, account4.getId())
 
             const events: any = await eventsPromise
             assert.equal(events.length, 1)
