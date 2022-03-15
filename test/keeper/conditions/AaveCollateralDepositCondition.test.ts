ea0a8a7c5e1b704faa27348b5657f08210ad85c2
ssallam <36411297+ssallam@users.noreply.github.com>
2022-03-15 11:18:04 +0100

    unittests for aave agreement conditions

--- test/keeper/conditions/AaveCollateralDepositCondition.test.ts
+++ test/keeper/conditions/AaveCollateralDepositCondition.test.ts
@@ -1,34 +1,73 @@
-// import { assert } from 'chai'
-// import { AaveCollateralDepositCondition } from '../../../src/keeper/contracts/conditions'
-// import { Nevermined } from '../../../src/nevermined/Nevermined'
-// import config from '../../config'
-// import TestContractHandler from '../TestContractHandler'
+import { assert } from 'chai'
+import { AaveCollateralDepositCondition } from '../../../src/keeper/contracts/conditions/'
+import { Nevermined } from '../../../src/nevermined/Nevermined'
+import config from '../../config'
+import TestContractHandler from '../TestContractHandler'
+import {zeroX} from "../../../src/utils";
+import BigNumber from "bignumber.js"
+import Account from "../../../src/nevermined/Account"
+import * as utils from "../../../src/utils"
+import DIDRegistry from "../../../src/keeper/contracts/DIDRegistry"
 
-// let condition: AaveCollateralDepositCondition
+let condition: AaveCollateralDepositCondition
 
 describe('AaveCollateralDepositCondition', () => {
-    // const did = `did:nv:${'a'.repeat(64)}`
-    // const vaultAddress = `0x${'a'.repeat(40)}`
-    // const assetToBorrow = `0x${'a'.repeat(40)}`
-    // const amount = 100
-    // const interestRateMode = 1
+    let did: string
+    let agreementId: string
+    const vaultAddress = `0x${'a'.repeat(40)}`
+    const assetToDeposit = `0x${'b'.repeat(40)}`
+    const assetToBorrow = `0x${'a'.repeat(40)}`
+    const depositAmount = new BigNumber('7000000000000000000')
+    const borrowAmount = new BigNumber('11000000000000000000')
+    const interestRateMode = 1
+    let user: Account
+    let nevermined: Nevermined
+    let didRegistry: DIDRegistry
 
     before(async () => {
-        // await TestContractHandler.prepareContracts()
-        // condition = (await Nevermined.getInstance(config)).keeper.conditions
-        //     .aaveCollateralDepositCondition
+        await TestContractHandler.prepareContracts()
+        nevermined = await Nevermined.getInstance(config)
+        ;[user, ] = await nevermined.accounts.list()
+        ;({ didRegistry } = nevermined.keeper)
+        condition = (await Nevermined.getInstance(config)).keeper.conditions
+            .aaveCollateralDepositCondition
     })
 
-    // describe('#hashValues()', () => {
-    //     it('should hash the values', async () => {
-    //         const hash = await condition.hashValues(
-    //             did,
-    //             vaultAddress,
-    //             assetToBorrow,
-    //             amount,
-    //             interestRateMode
-    //         )
-    //         assert.match(hash, /^0x[a-f0-9]{64}$/i)
-    //     })
-    // })
+    beforeEach(async () => {
+        agreementId = utils.generateId()
+        const didSeed = `did:nv:${utils.generateId()}`
+        did = await didRegistry.hashDID(didSeed, user.getId())
+    })
+
+    describe('#hashValues()', () => {
+        it('should hash the values', async () => {
+            const hash = await condition.hashValues(
+                zeroX(did),
+                vaultAddress,
+                assetToDeposit,
+                depositAmount,
+                assetToBorrow,
+                borrowAmount,
+                interestRateMode
+            )
+            assert.match(hash, /^0x[a-f0-9]{64}$/i)
+
+            const id = await condition.generateId(agreementId, hash)
+            assert.match(id, /^0x[a-f0-9]{64}$/i)
+
+            // const txReceipt: TransactionReceipt = await condition.fulfill(
+            //     agreementId,
+            //     did,
+            //     vaultAddress,
+            //     assetToDeposit,
+            //     depositAmount,
+            //     assetToBorrow,
+            //     borrowAmount,
+            //     interestRateMode
+            //     user
+            // )
+
+        })
+    })
+
 })
--- test/keeper/conditions/AaveRepayCondition.test.ts
+++ test/keeper/conditions/AaveRepayCondition.test.ts
@@ -1,33 +1,67 @@
-// import { assert } from 'chai'
-// import { AaveRepayCondition } from '../../../src/keeper/contracts/conditions/'
-// import { Nevermined } from '../../../src/nevermined/Nevermined'
-// import config from '../../config'
-// import TestContractHandler from '../TestContractHandler'
-//
-// let condition: AaveRepayCondition
-//
+import { assert } from 'chai'
+import { AaveRepayCondition } from '../../../src/keeper/contracts/conditions/'
+import { Nevermined } from '../../../src/nevermined/Nevermined'
+import config from '../../config'
+import TestContractHandler from '../TestContractHandler'
+import {zeroX} from "../../../src/utils";
+import BigNumber from "bignumber.js"
+import Account from "../../../src/nevermined/Account"
+import * as utils from "../../../src/utils"
+import DIDRegistry from "../../../src/keeper/contracts/DIDRegistry"
+
+let condition: AaveRepayCondition
+
 describe('AaveRepayCondition', () => {
-    //     const did = `did:nv:${'a'.repeat(64)}`
-    //     const vaultAddress = `0x${'a'.repeat(40)}`
-    //     const assetToBorrow = `0x${'a'.repeat(40)}`
-    //     const amount = 100
-    //     const interestRateMode = 1
-    //
-    //     before(async () => {
-    //         await TestContractHandler.prepareContracts()
-    //         condition = (await Nevermined.getInstance(config)).keeper.conditions
-    //             .aaveRepayCondition
-    //     })
-    // describe('#hashValues()', () => {
-    //     it('should hash the values', async () => {
-    //         const hash = await condition.hashValues(
-    //             did,
-    //             vaultAddress,
-    //             assetToBorrow,
-    //             amount,
-    //             interestRateMode
-    //         )
-    //         assert.match(hash, /^0x[a-f0-9]{64}$/i)
-    //     })
-    // })
+    let did: string
+    let agreementId: string
+    const vaultAddress = `0x${'a'.repeat(40)}`
+    const assetToRepay = `0x${'a'.repeat(40)}`
+    const amountToRepay = new BigNumber('11000000000000000000')
+    const interestRateMode = 1
+    let user: Account
+    let nevermined: Nevermined
+    let didRegistry: DIDRegistry
+
+    before(async () => {
+        await TestContractHandler.prepareContracts()
+        nevermined = await Nevermined.getInstance(config)
+        ;[user, ] = await nevermined.accounts.list()
+        ;({ didRegistry } = nevermined.keeper)
+        condition = (await Nevermined.getInstance(config)).keeper.conditions
+            .aaveRepayCondition
+    })
+
+    beforeEach(async () => {
+        agreementId = utils.generateId()
+        const didSeed = `did:nv:${utils.generateId()}`
+        did = await didRegistry.hashDID(didSeed, user.getId())
+    })
+
+    describe('#hashValues()', () => {
+        it('should hash the values', async () => {
+            const hash = await condition.hashValues(
+                zeroX(did),
+                vaultAddress,
+                assetToRepay,
+                amountToRepay,
+                interestRateMode
+            )
+            assert.match(hash, /^0x[a-f0-9]{64}$/i)
+
+            const id = await condition.generateId(agreementId, hash)
+            assert.match(id, /^0x[a-f0-9]{64}$/i)
+
+            // const txReceipt: TransactionReceipt = await condition.fulfill(
+            //     agreementId,
+            //     did,
+            //     vaultAddress,
+            //     assetToRepay,
+            //     amountToRepay,
+            //     interestRateMode
+            //     user
+            // )
+
+        })
+    })
+
 })
