bd994206c53c7f932bfcb7ef4563c15bb925d645
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-04-04 19:16:36 +0200

    Improved the way artifacts are copied

--- scripts/wait-nevermined-external.sh
+++ scripts/wait-nevermined-external.sh
@@ -2,16 +2,28 @@
 
 # Wait for contracts migration and extract Keeper artifacts
 
+RETRY_COUNT=0
+COMMAND_STATUS=1
+
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
 
-while [ ! -f "$HOME/.nevermined/nevermined-contracts/artifacts/ready" ]; do
-  sleep 2
+until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
+  nevermined_keeper_docker_id=$(docker container ls | grep nevermined-contracts | awk '{print $1}')
+  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready node_modules/@nevermined-io/contracts/artifacts/
+  COMMAND_STATUS=$?
+  sleep 5
+  let RETRY_COUNT=RETRY_COUNT+1
 done
+
+
+if [ $COMMAND_STATUS -ne 0 ]; then
+  echo "Waited for more than two minutes, but nevermined contracts have not been migrated yet. Did you run an Ethereum RPC client and the migration script?"
+  exit 1
+fi
 
 printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
-#rm $HOME/.nevermined/nevermined-contracts/artifacts/*.kovan.json
-mmv $HOME/.nevermined/nevermined-contracts/artifacts/\*.spree.json $HOME/.nevermined/nevermined-contracts/artifacts/\#1.kovan.json
-cp -rf $HOME/.nevermined/nevermined-contracts/artifacts/* node_modules/@nevermined-io/contracts/artifacts/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. node_modules/@nevermined-io/contracts/artifacts/
+for file in node_modules/@nevermined-io/contracts/artifacts/*.spree.json; do cp $file `echo $file | sed "s/.spree./.kovan./g"`; done
 
 printf '\e[32m✔ Copied new contract artifacts.\e[0m\n'
