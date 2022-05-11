24d28dcfc23414d8060cd77ee174f1872ec9796f
spieljs <spieljs@gmail.com>
2022-05-11 14:54:46 +0200

    fix aave tests

--- src/models/Config.ts
+++ src/models/Config.ts
@@ -7,6 +7,12 @@ export class Config {
      * MarketPlace URL.
      * @type {string}
      */
+    public metadataUri?: string
+
+    /**
+     * MarketPlace URL.
+     * @type {string}
+     */
     public marketplaceUri: string
 
     /**
