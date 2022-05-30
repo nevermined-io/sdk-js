9b6ae58ecfd2017e9c15b931f905cb2878afa8b8
spieljs <spieljs@gmail.com>
2022-05-30 12:37:46 +0200

    fix import

--- src/keeper/ContractHandler.ts
+++ src/keeper/ContractHandler.ts
@@ -2,7 +2,7 @@ import { Contract } from 'web3-eth-contract'
 import { Instantiable, InstantiableConfig } from '../Instantiable.abstract'
 import * as KeeperUtils from './utils'
 import { KeeperError } from '../errors/KeeperError'
-import fs from 'fs'
+import * as fs from 'fs'
 
 export default class ContractHandler extends Instantiable {
     protected static getContract(what: string, networkId: number, address?: string) {
