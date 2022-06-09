6790f4fd961b57ff0169131995e7b71234a47277
Sami Mäkelä <mrsmkl@gmail.com>
2022-06-09 16:54:31 +0300

    gateway has to find artifacts too...

--- scripts/wait-nevermined-external.sh
+++ scripts/wait-nevermined-external.sh
@@ -10,10 +10,15 @@ done
 
 printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
+ls $HOME/.nevermined/nevermined-contracts/artifacts
+
 #rm $HOME/.nevermined/nevermined-contracts/artifacts/*.kovan.json
 #mmv $HOME/.nevermined/nevermined-contracts/artifacts/\*.spree.json $HOME/.nevermined/nevermined-contracts/artifacts/\#1.kovan.json
 #cp -rf $HOME/.nevermined/nevermined-contracts/artifacts/* node_modules/@nevermined-io/contracts/artifacts/
 rm -f node_modules/@nevermined-io/contracts/artifacts/*.kovan.json
 mmv $HOME/.nevermined/nevermined-contracts/artifacts/\*.spree.json node_modules/@nevermined-io/contracts/artifacts/\#1.kovan.json
+cp node_modules/@nevermined-io/contracts/artifacts/*.kovan.json $HOME/.nevermined/nevermined-contracts/artifacts
+
+ls $HOME/.nevermined/nevermined-contracts/artifacts
 
 printf '\e[32m✔ Copied new contract artifacts.\e[0m\n'
