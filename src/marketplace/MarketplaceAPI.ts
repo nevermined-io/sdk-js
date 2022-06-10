26d5240ecf393b374d06bd9348450bac8fedf908
spieljs <spieljs@gmail.com>
2022-06-10 13:49:31 +0200

    add address endpoint

--- src/marketplace/MarketplaceAPI.ts
+++ src/marketplace/MarketplaceAPI.ts
@@ -36,4 +36,36 @@ export class MarketplaceApi extends Instantiable {
           throw new ApiError(error)
       }
     }
+
+    // prettier-ignore
+    public async addNewAddress(clientAssertion: string): Promise<string> {
+        const payload = {
+            'client_assertion_type':
+                'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+            'client_assertion': clientAssertion
+        }
+
+        try {
+            const response = await this.nevermined.utils.fetch.post(
+                `${this.url}${authPath}/address`,
+                JSON.stringify(payload),
+                {
+                    Authorization: `Bearer ${this.config.marketplaceAuthToken}`
+                }
+            )
+
+            if (!response.ok) {
+                throw new HttpError(
+                    `Error add address - ${response.statusText} ${response.url}`,
+                    response.status
+                )
+            }
+
+            this.config.marketplaceAuthToken = (await response.json()).access_token
+
+            return this.config.marketplaceAuthToken
+        } catch (error) {
+            throw new ApiError(error)
+        }
+    }
 }
