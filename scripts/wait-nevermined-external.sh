f202f187e710ecd61833a316bc088a82c6200fd9
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-06-21 10:01:52 +0200

    debugging

--- scripts/wait-nevermined-external.sh
+++ scripts/wait-nevermined-external.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+set -x
+set -e
 # Wait for contracts migration and extract Keeper artifacts
 
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
@@ -13,14 +14,7 @@ printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
 ls $HOME/.nevermined/nevermined-contracts/artifacts
 
-#rm $HOME/.nevermined/nevermined-contracts/artifacts/*.kovan.json
-#mmv $HOME/.nevermined/nevermined-contracts/artifacts/\*.spree.json $HOME/.nevermined/nevermined-contracts/artifacts/\#1.kovan.json
-#cp -rf $HOME/.nevermined/nevermined-contracts/artifacts/* node_modules/@nevermined-io/contracts/artifacts/
-rm -f node_modules/@nevermined-io/contracts/artifacts/*.kovan.json
 mmv $HOME/.nevermined/nevermined-contracts/artifacts/\*.spree.json node_modules/@nevermined-io/contracts/artifacts/\#1.kovan.json
-mmv node_modules/@nevermined-io/contracts/artifacts/\*.spree.json node_modules/@nevermined-io/contracts/artifacts/\#1.kovan.json
 cp node_modules/@nevermined-io/contracts/artifacts/*.kovan.json $HOME/.nevermined/nevermined-contracts/artifacts
-
-ls $HOME/.nevermined/nevermined-contracts/artifacts
 
 printf '\e[32m✔ Copied new contract artifacts.\e[0m\n'
