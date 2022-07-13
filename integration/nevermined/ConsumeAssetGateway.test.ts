36c9bfeb5da648a57e5b2a61a384b70f62676e40
Sami Mäkelä <mrsmkl@gmail.com>
2022-07-13 18:48:14 +0300

    changing here too

--- integration/nevermined/ConsumeAssetGateway.test.ts
+++ integration/nevermined/ConsumeAssetGateway.test.ts
@@ -88,8 +88,12 @@ describe('Consume Asset (Gateway)', () => {
 
     it('should get the lockPayment condition fulfilled', async () => {
         // todo change this, a test should never dependent on the previous test because the order might change during runtime
-        await new Promise(resolve => setTimeout(resolve, 500))
-        const status = await nevermined.agreements.status(agreementId)
+        let status
+        try {
+            status = await nevermined.agreements.status(agreementId)
+        } catch (err) {
+            status = await nevermined.agreements.status(agreementId)
+        }
 
         assert.deepEqual(status, {
             lockPayment: ConditionState.Fulfilled,
