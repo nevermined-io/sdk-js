2392eb529644f76e8c4eb35aeafbd9a0ddca7110
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-07-26 16:53:18 +0200

    Integration tests for CustomToken

---
+++ integration/nevermined/CustomToken.test.ts
@@ -0,0 +1,49 @@
+import { assert } from 'chai'
+import { Account, Nevermined } from '../../src'
+import CustomToken from '../../src/keeper/contracts/CustomToken'
+import { config } from '../config'
+
+describe('CustomToken', () => {
+    let account: Account
+    let nevermined: Nevermined
+    let erc20TokenAddress: string
+    let customErc20Token: CustomToken
+
+    before(async () => {
+        nevermined = await Nevermined.getInstance(config)
+        ;[account] = await nevermined.accounts.list()
+        erc20TokenAddress = nevermined.token.getAddress()
+    })
+
+    it('should get a custom token instance', async () => {
+        customErc20Token = await nevermined.contracts.loadErc20(erc20TokenAddress)
+        assert.isDefined(customErc20Token)
+    })
+
+    it('should get the token symbol', async () => {
+        const tokenSymbol = await customErc20Token.symbol()
+        console.log('tokenSymbol', tokenSymbol)
+        assert.equal(tokenSymbol, await nevermined.keeper.token.symbol())
+    })
+
+    it('should get the token name', async () => {
+        const tokenName = await customErc20Token.name()
+        console.log('tokenName', tokenName)
+        assert.equal(tokenName, await nevermined.keeper.token.name())
+    })
+
+    it('should get the token decimals', async () => {
+        const tokenDecimals = await customErc20Token.decimals()
+        console.log('tokenDecimals', tokenDecimals)
+        assert.equal(tokenDecimals, await nevermined.keeper.token.decimals())
+    })
+
+    it('should get the token balance of an account', async () => {
+        const tokenBalance = await customErc20Token.balanceOf(account.getId())
+        console.log('tokenBalance', tokenBalance)
+        assert.equal(
+            tokenBalance,
+            await nevermined.keeper.token.balanceOf(account.getId())
+        )
+    })
+})
