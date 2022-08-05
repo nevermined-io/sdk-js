a0cad2f1231c7a24e98b0a31c0f198188413e340
Aitor <1726644+aaitor@users.noreply.github.com>
2022-08-05 16:14:10 +0200

    Class to interact with Subscription NFTs (ERC-721) contract

---
+++ src/keeper/contracts/SubscriptionNft721.ts
@@ -0,0 +1,25 @@
+import { TxParameters } from './ContractBase'
+import { InstantiableConfig } from '../../Instantiable.abstract'
+import { abi } from './../../artifacts/NFT721SubscriptionUpgradeable.json'
+import { ethers } from 'ethers'
+import Nft721 from './Nft721'
+
+export default class SubscriptionNft721 extends Nft721 {
+    
+    public static async getInstance(
+        config: InstantiableConfig,
+        address: string
+    ): Promise<SubscriptionNft721> {
+        const nft: SubscriptionNft721 = new SubscriptionNft721('NFT721')
+        nft.setInstanceConfig(config)
+
+        await nft.checkExists(address)
+        nft.contract = new ethers.Contract(address, abi, nft.web3)
+
+        return nft
+    }
+
+    public async addMinter(newMinter: string, from: string, params?: TxParameters) {
+        return this.send('addMinter', from, [newMinter], params)
+    }
+}
