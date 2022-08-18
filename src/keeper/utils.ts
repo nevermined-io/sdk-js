906cbecfba281e300284e766ccb606afd62e2b2a
Sami Mäkelä <mrsmkl@gmail.com>
2022-08-18 14:50:23 +0300

    trying to get geth running

--- src/keeper/utils.ts
+++ src/keeper/utils.ts
@@ -28,6 +28,8 @@ export async function getNetworkName(networkId: number): Promise<string> {
             return 'Spree'
         case 8997:
             return 'polygon-localnet'
+        case 8998:
+            return 'geth-localnet'
         case 42220:
             return 'celo'
         case 44787:
