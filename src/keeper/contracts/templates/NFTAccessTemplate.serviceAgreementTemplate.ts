f3aec3edbff5c87733f7b4403e0b5e7580aa515c
Sami Mäkelä <mrsmkl@gmail.com>
2022-09-06 16:17:39 +0300

    refactoring service agreement templates

--- src/keeper/contracts/templates/NFTAccessTemplate.serviceAgreementTemplate.ts
+++ src/keeper/contracts/templates/NFTAccessTemplate.serviceAgreementTemplate.ts
@@ -1,4 +1,5 @@
 import { ServiceAgreementTemplate } from '../../../ddo/ServiceAgreementTemplate'
+import { nftAccessCondition, nftHolderTemplate } from './ConditionTemplates'
 
 export const nftAccessTemplateServiceAgreementTemplate: ServiceAgreementTemplate = {
     contractName: 'NFTAccessTemplate',
@@ -18,80 +19,5 @@ export const nftAccessTemplateServiceAgreementTemplate: ServiceAgreementTemplate
         nftHolder: [],
         nftAccess: []
     },
-    conditions: [
-        {
-            name: 'nftHolder',
-            timelock: 0,
-            timeout: 0,
-            contractName: 'NFTHolderCondition',
-            functionName: 'fulfill',
-            parameters: [
-                {
-                    name: '_did',
-                    type: 'bytes32',
-                    value: ''
-                },
-                {
-                    name: '_holderAddress',
-                    type: 'address',
-                    value: ''
-                },
-                {
-                    name: '_numberNfts',
-                    type: 'uint256',
-                    value: ''
-                }
-            ],
-            events: [
-                {
-                    name: 'Fulfilled',
-                    actorType: 'publisher',
-                    handler: {
-                        moduleName: 'nftHolderCondition',
-                        functionName: 'fulfillNFTHolderCondition',
-                        version: '0.1'
-                    }
-                }
-            ]
-        },
-        {
-            name: 'nftAccess',
-            timelock: 0,
-            timeout: 0,
-            contractName: 'NFTAccessCondition',
-            functionName: 'fulfill',
-            parameters: [
-                {
-                    name: '_documentId',
-                    type: 'bytes32',
-                    value: ''
-                },
-                {
-                    name: '_grantee',
-                    type: 'address',
-                    value: ''
-                }
-            ],
-            events: [
-                {
-                    name: 'Fulfilled',
-                    actorType: 'publisher',
-                    handler: {
-                        moduleName: 'nftAccess',
-                        functionName: 'fulfillNFTAccessCondition',
-                        version: '0.1'
-                    }
-                },
-                {
-                    name: 'TimedOut',
-                    actorType: 'consumer',
-                    handler: {
-                        moduleName: 'access',
-                        functionName: 'fulfillNFTAccessCondition',
-                        version: '0.1'
-                    }
-                }
-            ]
-        }
-    ]
+    conditions: [nftHolderTemplate(), nftAccessCondition()],
 }
