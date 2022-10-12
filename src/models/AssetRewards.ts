90c4dbb5529658f23103b13d039867be012abfbb
Aitor <1726644+aaitor@users.noreply.github.com>
2022-10-12 17:32:20 +0200

    Adding utility functions to calculate networkFees

--- src/models/AssetRewards.ts
+++ src/models/AssetRewards.ts
@@ -1,6 +1,8 @@
 import BigNumber from '../utils/BigNumber'
 
 export default class AssetRewards {
+    public static readonly NETWORK_FEE_DENOMINATOR = BigNumber.from(10000)
+
     private totalPrice: BigNumber
 
     private rewards: Map<string, BigNumber>
@@ -16,9 +18,10 @@ export default class AssetRewards {
     public constructor(..._params: any[]) {
         this.totalPrice = BigNumber.from(0)
         this.rewards = new Map()
+
         if (_params.length === 1) {
             const [rewards] = _params
-            this.rewards = rewards
+            this.rewards = rewards as Map<string, BigNumber>
             this.rewards.forEach(v => (this.totalPrice = this.totalPrice.add(v)))
         } else if (_params.length === 2) {
             const [address, amount] = _params
@@ -47,12 +50,71 @@ export default class AssetRewards {
         return [...Array.from(this.rewards.keys())]
     }
 
+    public setReceiver(receiver: string, amount: BigNumber): AssetRewards {
+        this.rewards.set(receiver, amount)
+        this.totalPrice = AssetRewards.sumAmounts(this.getAmounts())
+        return this
+    }
+
     public getAmountsString(): string {
         return this.rewards.size
             ? JSON.stringify(
                   Array.from(this.rewards.values()).map(value => value.toString())
               )
             : '[]'
+    }
+
+    /**
+     * It adds network fees on top of the already configured asset rewards
+     * @param feeReceiver - the address receiving the fees
+     * @param networkFeePercent - the percent of fees to receive, it uses the contract denomitor @see AssetRewards.NETWORK_FEE_DENOMINATOR
+     * @returns the asset rewards object
+     */
+    public addNetworkFees(
+        feeReceiver: string,
+        networkFeePercent: BigNumber
+    ): AssetRewards {
+        return this.setReceiver(
+            feeReceiver,
+            this.totalPrice
+                .mul(networkFeePercent)
+                .div(AssetRewards.NETWORK_FEE_DENOMINATOR)
+                .div(100)
+        )
+    }
+
+    /**
+     * It includes network fees on the existing asset rewards substracting the proportion taking into account the receivers percent
+     * @param feeReceiver - the address receiving the fees
+     * @param networkFeePercent - the percent of fees to receive, it uses the contract denomitor @see AssetRewards.NETWORK_FEE_DENOMINATOR
+     * @returns the asset rewards object
+     */
+    public adjustToIncludeNetworkFees(
+        feeReceiver: string,
+        networkFeePercent: BigNumber
+    ): AssetRewards {
+        const feesToInclude = this.totalPrice
+            .mul(networkFeePercent)
+            .div(AssetRewards.NETWORK_FEE_DENOMINATOR)
+            .div(100)
+
+        const newRewards: Map<string, BigNumber> = new Map()
+        this.rewards.forEach((k, v) => {
+            newRewards.set(
+                v,
+                k.sub(
+                    k
+                        .mul(networkFeePercent)
+                        .div(AssetRewards.NETWORK_FEE_DENOMINATOR)
+                        .div(100)
+                )
+            )
+        })
+        newRewards.set(feeReceiver, feesToInclude)
+        this.rewards = newRewards
+        this.totalPrice = AssetRewards.sumAmounts(this.getAmounts())
+
+        return this
     }
 
     public getReceiversString(): string {
