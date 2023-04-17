f3bdbf15f692ab4170845642e693f8c9b1c1065c
Aitor <1726644+aaitor@users.noreply.github.com>
2023-04-17 16:47:29 +0200

    ci: adding more nft 721 balance tests

--- integration/nevermined/Subscriptions.e2e.test.ts
+++ integration/nevermined/Subscriptions.e2e.test.ts
@@ -124,6 +124,8 @@ describe('Subscriptions using NFT ERC-721 End-to-End', () => {
 
       await subscriptionNFT.grantOperatorRole(transferNft721Condition.address, editor)
 
+      assert.isTrue(BigNumber.from(0).eq(await subscriptionNFT.balanceOf(editor.getId())))
+
       const isOperator = await subscriptionNFT.getContract.isOperator(
         transferNft721Condition.address,
       )
@@ -140,6 +142,8 @@ describe('Subscriptions using NFT ERC-721 End-to-End', () => {
         royaltyAttributes: royaltyAttributes,
       })
       subscriptionDDO = await nevermined.nfts721.create(nftAttributes, editor)
+
+      assert.isTrue(BigNumber.from(0).eq(await subscriptionNFT.balanceOf(editor.getId())))
 
       assert.isDefined(subscriptionDDO)
     })
@@ -185,6 +189,8 @@ describe('Subscriptions using NFT ERC-721 End-to-End', () => {
     it('The seller can check the payment and transfer the NFT to the subscriber', async () => {
       // Let's use the Node to mint the subscription and release the payments
 
+      assert.isTrue(BigNumber.from(0).eq(await subscriptionNFT.balanceOf(subscriber.getId())))
+
       const receipt = await nevermined.nfts721.claim(
         agreementId,
         editor.getId(),
@@ -196,6 +202,8 @@ describe('Subscriptions using NFT ERC-721 End-to-End', () => {
         await nevermined.nfts721.ownerOfAssetByAgreement(subscriptionDDO.shortId(), agreementId),
         subscriber.getId(),
       )
+
+      assert.isTrue(BigNumber.from(1).eq(await subscriptionNFT.balanceOf(subscriber.getId())))
     })
 
     it('the editor and reseller can receive their payment', async () => {
