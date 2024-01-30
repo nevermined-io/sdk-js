fb1e6eeedc0a544ed39060542cdccc19ef87d374
Aitor <1726644+aaitor@users.noreply.github.com>
2024-01-30 11:37:01 +0100

    fix: transfer ownership metadata

--- integration/nevermined/AssetOwners.test.ts
+++ integration/nevermined/AssetOwners.test.ts
@@ -15,27 +15,35 @@ import { mineBlocks } from '../utils/utils'
 describe('Asset Owners', () => {
   let nevermined: Nevermined
   let nevermined2: Nevermined
+  let nevermined3: Nevermined
   let config2: NeverminedOptions
+  let config3: NeverminedOptions
 
   let account1: Account
   let account2: Account
+  let account3: Account
 
   let newMetadata: (token: string) => MetaData
 
   before(async () => {
     config2 = { ...config }
+    config3 = { ...config }
     nevermined = await Nevermined.getInstance(config)
     nevermined2 = await Nevermined.getInstance(config2)
+    nevermined3 = await Nevermined.getInstance(config3)
 
     // Accounts
-    ;[account1, account2] = await nevermined.accounts.list()
+    ;[account1, account2, account3] = await nevermined.accounts.list()
 
     const clientAssertion = await nevermined.utils.jwt.generateClientAssertion(account1)
 
     const clientAssertion2 = await nevermined2.utils.jwt.generateClientAssertion(account2)
 
+    const clientAssertion3 = await nevermined2.utils.jwt.generateClientAssertion(account3)
+
     await nevermined.services.marketplace.login(clientAssertion)
     await nevermined2.services.marketplace.login(clientAssertion2)
+    await nevermined3.services.marketplace.login(clientAssertion3)
 
     newMetadata = (token: string) => {
       const metadata = getMetadata()
@@ -111,18 +119,42 @@ describe('Asset Owners', () => {
     assert.includeMembers(result, [ddo.id])
   })
 
-  it('should be able to transfer ownership', async () => {
-    const { id } = await nevermined.assets.create(
-      AssetAttributes.getInstance({
-        metadata: newMetadata(config.marketplaceAuthToken),
-      }),
-      account1,
-    )
+  describe('Ownership transfer', () => {
+    let did: string
+    it('should be able to transfer ownership to account2', async () => {
+      const assetDDO = await nevermined.assets.create(
+        AssetAttributes.getInstance({
+          metadata: newMetadata(config.marketplaceAuthToken),
+        }),
+        account1,
+      )
+      did = assetDDO.id
+      assert.equal(assetDDO._nvm.versions.length, 1)
+      // transfer
+      await nevermined.assets.transferOwnership(did, account2.getId(), account1)
+      const newOwner = await nevermined.keeper.didRegistry.getDIDOwner(did)
 
-    // transfer
-    await nevermined.assets.transferOwnership(id, account2.getId(), account1)
-    const newOwner = await nevermined.keeper.didRegistry.getDIDOwner(id)
+      assert.equal(newOwner, account2.getId())
 
-    assert.equal(newOwner, account2.getId())
+      const ddo = await nevermined.assets.resolve(did)
+      assert.equal(ddo._nvm.versions.length, 2)
+
+      assert.equal(ddo.publicKey[0].owner, account2.getId())
+      assert.equal(ddo.proof.creator, account2.getId())
+    })
+
+    it('account2 should be able to transfer ownership to account3', async () => {
+      // transfer
+      await nevermined2.assets.transferOwnership(did, account3.getId(), account2)
+      const newOwner = await nevermined.keeper.didRegistry.getDIDOwner(did)
+
+      assert.equal(newOwner, account3.getId())
+
+      const ddo = await nevermined.assets.resolve(did)
+      assert.equal(ddo._nvm.versions.length, 3)
+
+      assert.equal(ddo.publicKey[0].owner, account3.getId())
+      assert.equal(ddo.proof.creator, account3.getId())
+    })
   })
 })
