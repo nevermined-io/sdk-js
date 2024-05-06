59b2ada3fb78b67614a25d203be86ceb6bab943b
enrique <enrique@nevermined.io>
2024-05-06 13:37:43 +0200

    fix: remove console.log

--- src/keeper/contracts/ContractBase.ts
+++ src/keeper/contracts/ContractBase.ts
@@ -130,7 +130,6 @@ export abstract class ContractBase extends Instantiable {
     args: any[],
     params: TxParameters = {},
   ) {
-    console.log(functionName, from)
     const signer = from.getAccountSigner()
     if (from.getType() === 'local') {
       this.logger.debug(
